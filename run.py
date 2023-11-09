37c85fca8684cf5d1bc68c1c0904093a54519f75
DMASCoreDeclan <declan.sweeney@gmail.com>
2023-11-09 10:42:30 +0000

    MAking it work!

---
+++ run.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
